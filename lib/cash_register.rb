87e279d0d52d4bc5671af0980734638f334626aa
Chad_Barnes <Chad.A.Barnes@outlook.com>
2020-08-17 16:40:18 -0400

    Passinbg Rspec

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
 
+class CashRegister	
+
+
+    attr_accessor :discount, :total, :latest_add, :items
+  
+    def initialize(discount=0)	
+      @discount = discount	
+      @total = 0	
+      @items = []
+    end
+
+    def add_item (item, price, quantity =1)
+        @total += price * quantity
+        @latest_add = [price, quantity, item]
+        quantity.times {@items << item } 
+    end
+
+    def apply_discount
+            @discount.zero?() ? "There is no discount to apply." : "After the discount, the total comes to $#{@total -= (@total *@discount/100)}."
+    end
+
+    def void_last_transaction
+        @total -= @latest_add[0] * @latest_add[1]
+        @latest_add[1].times {@items.pop}
+    end
+
+
+end
